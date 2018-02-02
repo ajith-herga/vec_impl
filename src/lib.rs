e116ad3fbd358fd0cbf858472cac3dfc78586557
Ajith <ajkedlaya@gmail.com>
2018-02-01 23:40:18 -0800

    Add Reallocate within Vec
    Use layout instead of capacity.

--- src/lib.rs
+++ src/lib.rs
@@ -6,27 +6,40 @@ use std::heap::{Alloc, Heap, Layout};
 
 pub struct Vec<T> {
 vec: Unique<T>,
-capacity: u32,
-len: u32,
+layout: Layout,
+len: usize,
 }
 
 impl<T> Vec<T> {
     fn new() -> Self {
-        Vec { vec: Unique::empty(), capacity: 0, len: 0 }
+        Vec { vec: Unique::empty(), layout: Layout::new::<()>(), len: 0 }
     }
 
     // TODO deallocate? drop will be called.
     fn insert(&mut self, elem: T) {
         // if full, alloc
-        if self.len == self.capacity {
+        if self.len == self.layout.size() {
             // Allocate one size if len is 0
-            if self.len == 0 {
+            if self.layout.size() == 0 {
                 unsafe {
-                    let ptr = Heap.alloc(Layout::array::<T>(32).unwrap()).unwrap();
+                    let layout = Layout::array::<T>(32).unwrap();
+                    let ptr = Heap.alloc(layout.clone()).unwrap();
+                    self.layout = layout;
+                    self.vec = Unique::new_unchecked(mem::transmute(ptr));
+                }
+            } else {
+            // Reallocate if size is not zero.
+                unsafe {
+                    let layout = Layout::array::<T>(self.layout.size()*2).unwrap();
+                    let ptr = Heap.realloc(
+                        mem::transmute(self.vec.as_ptr()),
+                        self.layout.clone(),
+                        layout.clone()
+                        ).unwrap();
+                    self.layout = layout;
                     self.vec = Unique::new_unchecked(mem::transmute(ptr));
                 }
             }
-            // Reallocate if size is not zero.
         }
         // write
     }
