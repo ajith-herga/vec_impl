c5e104812e7029b642541d6a413ef2ecd99c06c8
Ajith <ajkedlaya@gmail.com>
2018-02-10 16:35:41 -0800

    Add drop, verify with tests

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# Things to do.
+## Complete the API
+    More work to do with the current API: Remove from front, from back and so on
+## New datastrutures
+    * Deque? The new interest.
+    * Composable DS: LRU cache?
--- src/lib.rs
+++ src/lib.rs
@@ -8,14 +8,25 @@ pub struct MyVec<T> {
     my_vec: Unique<T>,
     layout: Layout,
     len: usize,
+    reserve: usize,
 }
 
 impl<T> MyVec<T> {
-    pub fn new() -> Self {
-        MyVec {
-            my_vec: Unique::empty(),
-            layout: Layout::new::<()>(),
-            len: 0,
+    pub fn new(reserve: Option<usize>) -> Self {
+        if let Some(reserve) = reserve {
+            MyVec {
+                my_vec: Unique::empty(),
+                layout: Layout::new::<()>(),
+                len: 0,
+                reserve,
+            }
+        } else {
+            MyVec {
+                my_vec: Unique::empty(),
+                layout: Layout::new::<()>(),
+                len: 0,
+                reserve: 32,
+            }
         }
     }
 
@@ -23,7 +34,7 @@ impl<T> MyVec<T> {
         // Allocate one size if len is 0
         if self.layout.size() == 0 {
             unsafe {
-                let layout = Layout::array::<T>(32).unwrap();
+                let layout = Layout::array::<T>(self.reserve).unwrap();
                 let ptr = Heap.alloc(layout.clone())?;
                 self.layout = layout;
                 self.my_vec = Unique::new_unchecked(mem::transmute(ptr));
@@ -31,7 +42,7 @@ impl<T> MyVec<T> {
         } else {
             // Reallocate if size is not zero.
             unsafe {
-                let layout = Layout::array::<T>(self.layout.size() * 2).unwrap();
+                let layout = self.layout.extend(self.layout.clone()).unwrap().0;
                 let ptr = Heap.realloc(
                     mem::transmute(self.my_vec.as_ptr()),
                     self.layout.clone(),
@@ -69,33 +80,29 @@ impl<T> MyVec<T> {
     }
 }
 
+impl<T> Drop for MyVec<T> {
+    fn drop(&mut self) {
+        for index in 0..self.len {
+            unsafe {
+                ptr::read(self.my_vec.as_ptr().offset(index as isize));
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::MyVec;
     use std::mem::size_of;
 
     #[test]
-    fn test_vec_i32() {
-        let mut my_vec: MyVec<i32> = MyVec::new();
-        my_vec.push_back(15).unwrap();
-        my_vec.push_back(0).unwrap();
-        my_vec.push_back(-150).unwrap();
-        assert_eq!(my_vec.layout.size(), 32 * size_of::<i32>());
-        assert_eq!(my_vec.get(0), Some(&15));
-        assert_eq!(my_vec.get(1), Some(&0));
-        assert_eq!(my_vec.get(2), Some(&-150));
-    }
-
-    #[test]
     fn test_vec_int() {
         let ints = vec![1, 2, 3, 4, 5];
-        let mut my_vec: MyVec<i32> = MyVec::new();
+        let mut my_vec: MyVec<i32> = MyVec::new(None);
 
         for elem in ints.iter() {
             my_vec.push_back(*elem).unwrap();
         }
-
-        assert_eq!(my_vec.layout.size(), 32 * size_of::<i32>());
 
         for i in 0..30 {
             assert_eq!(my_vec.get(i), ints.get(i));
@@ -105,33 +112,50 @@ mod tests {
     #[test]
     fn test_vec_str() {
         let strings = vec!["So", "far", "so", "good"];
-        let mut my_vec: MyVec<&str> = MyVec::new();
+        let mut my_vec: MyVec<&str> = MyVec::new(None);
 
         for elem in strings.iter() {
             my_vec.push_back(elem).unwrap();
         }
-
-        assert_eq!(my_vec.layout.size(), 32 * size_of::<&str>());
 
         for i in 0..30 {
             assert_eq!(my_vec.get(i), strings.get(i));
         }
     }
 
+    #[derive(Debug)]
+    struct RT<'a> {
+        val: i32,
+        name: &'a str,
+    }
+
+    impl<'a> RT<'a> {
+        fn new(val: i32) -> Self {
+            RT { val, name: "Ajith" }
+        }
+    }
+
+    impl<'a> Drop for RT<'a> {
+        fn drop(&mut self) {
+            //println!("Called drop for RT ({}, {})", self.val, self.name);
+        }
+    }
+
+    #[test]
+    fn test_vec_rt_val() {
+        let mut my_vec: MyVec<RT> = MyVec::new(Some(2));
+
+        my_vec.push_back(RT::new(15)).unwrap();
+        my_vec.push_back(RT::new(150)).unwrap();
+        my_vec.push_back(RT::new(0)).unwrap();
+        my_vec.push_back(RT::new(-15)).unwrap();
+        my_vec.push_back(RT::new(-150)).unwrap();
+
+        assert_eq!(my_vec.layout.size(), 8 * size_of::<RT>());
+    }
+
     #[test]
     fn test_vec_rt_ref() {
-        #[derive(Debug)]
-        struct RT<'a> {
-            val: i32,
-            name: &'a str,
-        }
-
-        impl<'a> RT<'a> {
-            fn new(val: i32) -> Self {
-                RT { val, name: "Ajith" }
-            }
-        }
-
         let rts = vec![
             RT::new(15),
             RT::new(150),
@@ -139,13 +163,13 @@ mod tests {
             RT::new(-1),
             RT::new(150),
         ];
-        let mut my_vec: MyVec<&RT> = MyVec::new();
+        let mut my_vec: MyVec<&RT> = MyVec::new(Some(2));
 
         for elem in rts.iter() {
             my_vec.push_back(elem).unwrap();
         }
 
-        assert_eq!(my_vec.layout.size(), 32 * size_of::<&RT>());
+        assert_eq!(my_vec.layout.size(), 8 * size_of::<&RT>());
 
         for i in 0..rts.len() {
             assert_eq!(my_vec.get(i).unwrap().val, rts.get(i).unwrap().val);
