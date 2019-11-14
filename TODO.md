3dde9643846a0dcba2f9040e79bf1e5733556eab
Ajith <ajkedlaya@gmail.com>
2019-11-13 22:43:18 -0800

    Add general heap
    1. Possibly more cache and copy efficient, keys are different from
       value.
    2. A build up to frozen set implementation.
    3. Bug fixes for heap

--- TODO.md
+++ TODO.md
@@ -1,19 +1,22 @@
 # Things to do.
-## Complete the API
-    More work to do with the current API:
-    * Add benchmark tests
-    * Implement From Vec for MyVec, which gives Vec into Myvec.
-    * Macro with default/0 initialized vec.
-    * Implement FromIterator.
-    * Implement serialize, deserialize.
-    * Send, Sync?
-    * Return errors from operations that can fail.
+## Complete the Vec API
+More work to do with the current API:
+* Add benchmark tests
+* Implement From Vec for MyVec, which gives Vec into Myvec.
+* Macro with default/0 initialized vec.
+* Implement FromIterator.
+* Implement serialize, deserialize.
+* Send, Sync?
+* Return errors from operations that can fail.
+## Complete the heap API
+* Linear time setup from vector
 ## Integrate with TravisCI.
-    * Run tests, benchmarks, build.
-    * Figure kcov.
+* Run tests, benchmarks, build.
+* Figure kcov.
 ## New datastrutures
-    * Deque? The new interest.
-    * Simple nullable array vs circular buffer
-    * Composable DS: LRU cache?
-    * Hash, rebalance.. vector of vectors, constant search and insert times.
-    * Set, can use derefmut methods internally?
+* Frozen set from vector, cache efficient tree like.
+* Deque? The new interest.
+* Simple nullable array vs circular buffer
+* Composable DS: LRU cache?
+* Hash, rebalance.. vector of vectors, constant search and insert times.
+* Set, can use derefmut methods internally?
--- src/heap.rs
+++ src/heap.rs
@@ -1,12 +1,5 @@
 use vec::MyVec;
-use std::{fmt, vec::Vec};
-pub struct MyMaxHeap<T> {
-    my_heap: MyHeap<T>,
-}
-
-pub struct MyMinHeap<T> {
-    my_heap: MyHeap<T>,
-}
+use std::{fmt, vec::Vec, cmp};
 
 pub struct MyHeap<T> {
     my_vec: MyVec<T>,
@@ -93,7 +86,7 @@ impl<T: Ord> MyHeap<T> {
         return top;
     }
 
-    pub fn to_vec(mut self) -> Vec<T> {
+    pub fn to_vec(&mut self) -> Vec<T> {
         let mut list : Vec<T> = Vec::new();
         while let Some(elem) = self.pop() {
             list.push(elem);
@@ -118,6 +111,10 @@ impl<T: fmt::Display> fmt::Display for MyHeap<T> {
     }
 }
 
+pub struct MyMinHeap<T> {
+    my_heap: MyHeap<T>,
+}
+
 impl<T: Ord> MyMinHeap<T> {
     pub fn new() -> Self {
         MyMinHeap {
@@ -126,6 +123,7 @@ impl<T: Ord> MyMinHeap<T> {
     }
 
     pub fn add(&mut self, elem: T) {
+        self.my_heap.add(elem)
     }
 
     pub fn top(&self) -> Option<&T> {
@@ -137,18 +135,18 @@ impl<T: Ord> MyMinHeap<T> {
     }
 
     pub fn from_vec(elems: Vec<T>)  -> Self {
-        let mut heap: MyHeap<T> = MyHeap::new(false);
-        for elem in elems.into_iter() {
-            heap.add(elem);
-        }
         MyMinHeap {
-            my_heap: heap,
+            my_heap: MyHeap::from_vec(elems, false)
         }
     }
 
-    pub fn to_vec(mut self) -> Vec<T> {
+    pub fn to_vec(&mut self) -> Vec<T> {
         self.my_heap.to_vec()
     }
+}
+
+pub struct MyMaxHeap<T> {
+    my_heap: MyHeap<T>,
 }
 
 impl<T: Ord> MyMaxHeap<T> {
@@ -159,6 +157,7 @@ impl<T: Ord> MyMaxHeap<T> {
     }
 
     pub fn add(&mut self, elem: T) {
+        self.my_heap.add(elem)
     }
 
     pub fn top(&self) -> Option<&T> {
@@ -170,17 +169,155 @@ impl<T: Ord> MyMaxHeap<T> {
     }
 
     pub fn from_vec(elems: Vec<T>)  -> Self {
-        let mut heap: MyHeap<T> = MyHeap::new(true);
-        for elem in elems.into_iter() {
-            heap.add(elem);
-        }
         MyMaxHeap {
-            my_heap: heap,
+            my_heap: MyHeap::from_vec(elems, true)
         }
     }
 
-    pub fn to_vec(mut self) -> Vec<T> {
+    pub fn to_vec(&mut self) -> Vec<T> {
         self.my_heap.to_vec()
+    }
+}
+
+#[derive(Eq)]
+pub struct KeyWithOffset<K: Ord> {
+    pub key: K,
+    pub offset: usize,
+}
+
+impl<K: Ord> PartialEq for KeyWithOffset<K> {
+    fn eq(&self, other: &Self) -> bool {
+        other.key == self.key
+    }
+}
+
+impl<K: Ord> PartialOrd for KeyWithOffset<K> {
+    fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
+        return Some(self.cmp(other))
+    }
+}
+
+impl<K: Ord> Ord for KeyWithOffset<K> {
+    fn cmp(&self, other: &Self) -> cmp::Ordering {
+        return self.key.cmp(&other.key)
+    }
+}
+
+pub struct MyMaxHeapKeys<K: Ord, V> {
+    my_heap: MyMaxHeap<KeyWithOffset<K>>,
+    my_vec: MyVec<V>,
+}
+
+pub struct MyMinHeapKeys<K: Ord, V> {
+    my_heap: MyMinHeap<KeyWithOffset<K>>,
+    my_vec: MyVec<V>,
+}
+
+impl<K:Ord, V> MyMaxHeapKeys<K, V> {
+    pub fn new() -> Self {
+        MyMaxHeapKeys {
+            my_heap: MyMaxHeap::new(),
+            my_vec: MyVec::new(None),
+        }
+    }
+
+    pub fn add(&mut self, key: K, value: V) {
+        self.my_vec.push_back(value);
+        self.my_heap.add(KeyWithOffset {
+            key: key,
+            offset: self.my_vec.len() - 1,
+        })
+    }
+
+    pub fn top(&self) -> Option<(&K, &V)> {
+        self.my_heap.top().map(|k_off_ref| {
+        (&k_off_ref.key, self.my_vec.get(k_off_ref.offset).unwrap())})
+    }
+
+    pub fn pop(&mut self) -> Option<(K,V)> {
+        self.my_heap.pop().map(|k_off| {
+            let KeyWithOffset {key, offset} = k_off;
+            let len = self.my_vec.len();
+            assert!(len > 0);
+            self.my_vec.swap(offset, len - 1);
+            (key, self.my_vec.pop().unwrap())
+        })
+    }
+
+    pub fn from_vec(elems: Vec<(K,V)>)  -> Self {
+        let mut max_heap_keys = MyMaxHeapKeys {
+            my_heap : MyMaxHeap::new(),
+            my_vec: MyVec::new(None),
+        };
+
+        for elem in elems.into_iter() {
+            let (key, value) = elem;
+            max_heap_keys.add(key, value);
+        }
+
+        max_heap_keys
+    }
+
+    pub fn to_vec(&mut self) -> Vec<(K,V)> {
+        let mut ret_vec = Vec::new();
+        while let Some(elem) = self.pop() {
+            ret_vec.push(elem);
+        }
+        ret_vec
+    }
+}
+
+impl<K:Ord, V> MyMinHeapKeys<K, V> {
+    pub fn new() -> Self {
+        MyMinHeapKeys {
+            my_heap: MyMinHeap::new(),
+            my_vec: MyVec::new(None),
+        }
+    }
+
+    pub fn add(&mut self, key: K, value: V) {
+        self.my_vec.push_back(value);
+        self.my_heap.add(KeyWithOffset {
+            key: key,
+            offset: self.my_vec.len() - 1,
+        })
+    }
+
+    pub fn top(&self) -> Option<(&K, &V)> {
+        self.my_heap.top().map(|k_off_ref| {
+        (&k_off_ref.key, self.my_vec.get(k_off_ref.offset).unwrap())})
+    }
+
+    pub fn pop(&mut self) -> Option<(K,V)> {
+        self.my_heap.pop().map(|k_off| {
+            let KeyWithOffset {key, offset} = k_off;
+            let len = self.my_vec.len();
+            assert!(len > 0);
+            self.my_vec.swap(offset, len - 1);
+            (key, self.my_vec.pop().unwrap())
+        })
+    }
+
+    pub fn from_vec(elems: Vec<(K,V)>)  -> Self {
+        let mut max_heap_keys = MyMinHeapKeys {
+            my_heap : MyMinHeap::new(),
+            my_vec: MyVec::new(None),
+        };
+
+        for elem in elems.into_iter() {
+            let (key, value) = elem;
+            max_heap_keys.add(key, value);
+        }
+
+        max_heap_keys
+    }
+
+    pub fn to_vec(&mut self) -> Vec<(K,V)> {
+        let mut ret_vec = Vec::new();
+        while let Some(elem) = self.pop() {
+            ret_vec.push(elem);
+        }
+        ret_vec
     }
 }
 
@@ -188,6 +325,8 @@ impl<T: Ord> MyMaxHeap<T> {
 mod tests {
     use super::MyMaxHeap;
     use super::MyMinHeap;
+    use super::MyMinHeapKeys;
+    use super::MyMaxHeapKeys;
     #[test]
     fn test_none() {
         let mut heap: MyMaxHeap<i32> = MyMaxHeap::new();
@@ -202,9 +341,19 @@ mod tests {
     fn test_heap_sort() {
         let sorted_vec = vec![9, 8, 7, 6, 5, 4, 3, 2, 1];
         let rev_sorted_vec = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
-        let heap = MyMaxHeap::from_vec(rev_sorted_vec.clone());
+        let mut heap = MyMaxHeap::from_vec(rev_sorted_vec.clone());
         assert_eq!(heap.to_vec(), sorted_vec);
-        let heap = MyMinHeap::from_vec(sorted_vec);
+        let mut heap = MyMinHeap::from_vec(sorted_vec);
+        assert_eq!(heap.to_vec(), rev_sorted_vec);
+    }
+
+    #[test]
+    fn test_key_val_sort() {
+        let sorted_vec = vec![(9, 'j'), (8, 'i'), (7, 'h'), (6, 'g'), (5, 'f'), (4, 'e'), (3, 'd'), (2, 'c'), (1, 'b')];
+        let rev_sorted_vec = vec![(1, 'b'), (2, 'c'), (3, 'd'), (4, 'e'), (5, 'f'), (6, 'g'), (7, 'h'), (8, 'i'), (9, 'j')];
+        let mut heap = MyMaxHeapKeys::from_vec(rev_sorted_vec.clone());
+        assert_eq!(heap.to_vec(), sorted_vec);
+        let mut heap = MyMinHeapKeys::from_vec(sorted_vec);
         assert_eq!(heap.to_vec(), rev_sorted_vec);
     }
 }
