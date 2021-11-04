17b865502f587480c7a7e20ff13e839084492187
Ajith <ajkedlaya@gmail.com>
2021-11-04 00:49:53 -0700

    Setup bindgen to generate Rust FFI

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,14 @@
 mod vec;
 mod stack;
 mod heap;
+mod hash;
 
 pub use vec::MyVec;
 pub use stack::MyStack;
 pub use heap::MyMaxHeap;
 pub use heap::MyMinHeap;
+pub use hash::MyHashSet1;
+
+#[allow(non_camel_case_types)]
+#[allow(non_upper_case_globals)]
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
